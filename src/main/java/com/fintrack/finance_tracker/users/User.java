c7433c9df4c024e2b6b6b962e6695a09b114005e
Z33xD <zeeshanmoinshariff@gmail.com>
2025-12-20 00:14:29 +0530

    Creating a new file for UserController. Will do the API Mappings later. Also cleaned up User class

--- src/main/java/com/fintrack/finance_tracker/users/User.java
+++ src/main/java/com/fintrack/finance_tracker/users/User.java
@@ -3,7 +3,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import org.springframework.cglib.core.Local;
 
 import java.time.LocalDateTime;
 
