cd2dcd7c11fb3bd6bee665883d4582398e14a739
Z33xD <zeeshanmoinshariff@gmail.com>
2025-12-30 10:19:05 +0530

    Added basic routes to `UserController` class

--- src/main/java/com/fintrack/finance_tracker/users/UserController.java
+++ src/main/java/com/fintrack/finance_tracker/users/UserController.java
@@ -1,8 +1,65 @@
 package com.fintrack.finance_tracker.users;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
+@RequestMapping(path = "/api/users/")
 public class UserController {
+    private final UserService userService;
 
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getUsers(
+            @RequestParam(required = false) Integer id,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email
+    ) {
+        if (id != null) {
+            return userService.getUserById(id)
+                    .map(List::of)
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        }
+        else if (name != null) {
+            return userService.getUsersByName(name);
+        }
+        else if (email != null) {
+            return userService.getUsersByEmail(email);
+        }
+        else {
+            return userService.getUsers();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        User createdUser = userService.addUser(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+        User resultUser = userService.updateUser(user);
+        if (resultUser != null) {
+            return new ResponseEntity<>(resultUser, HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/delete/{userId}")
+    public ResponseEntity<String> deleteUser(@PathVariable int userId) {
+        userService.deleteUser(userId);
+        return new ResponseEntity<>("User deleted successfully!", HttpStatus.OK);
+    }
 }
