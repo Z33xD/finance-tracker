3f94474fc0ae76f9bed1b3bc382abf72a6708252
Z33xD <zeeshanmoinshariff@gmail.com>
2026-01-03 01:30:12 +0530

    Setup all routes in `TransactionController` class except for the "`/me`" routes. Also updated the `updateTransaction` method in the `TransactionService` class.

--- src/main/java/com/fintrack/finance_tracker/transactions/TransactionService.java
+++ src/main/java/com/fintrack/finance_tracker/transactions/TransactionService.java
@@ -37,23 +37,71 @@ public class TransactionService {
         return transaction;
     }
 
-    public Transaction updateTransaction(Transaction updatedTransaction) {
-        Optional<Transaction> existingTransaction = transactionRepository.findById(updatedTransaction.getId());
+    public Transaction updateTransaction(int id, Transaction updatedTransaction) {
+        Optional<Transaction> existingTransaction = transactionRepository.findById(id);
 
         if (existingTransaction.isPresent()) {
             Transaction transactionToUpdate = existingTransaction.get();
             transactionToUpdate.setAccount_id(updatedTransaction.getAccount_id());
+
             transactionToUpdate.setCategory_id(updatedTransaction.getCategory_id());
+
             transactionToUpdate.setAmount(updatedTransaction.getAmount());
-            transactionToUpdate.setTransaction_date(updatedTransaction.getTransaction_date());
-            transactionToUpdate.setDescription(updatedTransaction.getDescription());
-            transactionToUpdate.setTransaction_type(updatedTransaction.getTransaction_type());
+
+            if (updatedTransaction.getTransaction_date() != null) {
+                transactionToUpdate.setTransaction_date(updatedTransaction.getTransaction_date());
+            }
+
+            if (updatedTransaction.getDescription() != null) {
+                transactionToUpdate.setDescription(updatedTransaction.getDescription());
+            }
+
+            if (updatedTransaction.getTransaction_type() != null) {
+                transactionToUpdate.setTransaction_type(updatedTransaction.getTransaction_type());
+            }
+
             transactionToUpdate.setImport_batch_id(updatedTransaction.getImport_batch_id());
 
             transactionRepository.save(transactionToUpdate);
             return transactionToUpdate;
         }
         return null;
+    }
+
+    public List<Transaction> searchTransactions(Integer category_id, LocalDate start_date, LocalDate end_date, String transaction_type) {
+        List<Transaction> transactions = transactionRepository.findAll();
+
+        if (category_id != null) {
+            transactions = transactions.stream()
+                    .filter(transaction -> (transaction.getCategory_id() == category_id))
+                    .collect(Collectors.toList());
+        }
+
+        if (transaction_type != null) {
+            transactions = transactions.stream()
+                    .filter(transaction -> transaction.getTransaction_type().equalsIgnoreCase(transaction_type))
+                    .collect(Collectors.toList());
+        }
+
+        if ((start_date != null) && (end_date != null)) {
+            transactions = transactions.stream()
+                    .filter(transaction -> ((!transaction.getTransaction_date().isBefore(start_date)) && (!transaction.getTransaction_date().isAfter(end_date))))
+                    .collect(Collectors.toList());
+        }
+
+        else if (start_date != null) {
+            transactions = transactions.stream()
+                    .filter(transaction -> !transaction.getTransaction_date().isBefore(start_date))
+                    .collect(Collectors.toList());
+        }
+
+        else if (end_date != null) {
+            transactions = transactions.stream()
+                    .filter(transaction -> !transaction.getTransaction_date().isAfter(end_date))
+                    .collect(Collectors.toList());
+        }
+
+        return transactions;
     }
 
     @Transactional
