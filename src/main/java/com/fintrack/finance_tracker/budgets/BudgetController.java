a6a6c91add7ab59e86a6b8c978e7a19910be23a7
Z33xD <zeeshanmoinshariff@gmail.com>
2026-01-03 00:50:42 +0530

    Added a method in `BudgetController` to retrieve a list of all budgets, for probable admin access

--- src/main/java/com/fintrack/finance_tracker/budgets/BudgetController.java
+++ src/main/java/com/fintrack/finance_tracker/budgets/BudgetController.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "api/budgets")
 public class BudgetController {
@@ -17,6 +19,27 @@ public class BudgetController {
     }
 
     // TODO: GET /api/budgets (Retrieve all budgets for the authenticated user)
+
+    @GetMapping
+    public List<Budget> getBudgets(
+            @RequestParam (required = false) Integer id,
+            @RequestParam (required = false) Integer month,
+            @RequestParam (required = false) Integer year
+    ) {
+        if (id != null) {
+            return budgetService.getBudgetById(id)
+                    .map(List::of)
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        }
+
+        else if ((month != null) && (year != null)) {
+            return budgetService.getBudgetsByMonthAndYear(month, year);
+        }
+
+        else {
+            return budgetService.getBudgets();
+        }
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<Budget> getBudgetById(@PathVariable int id) {
