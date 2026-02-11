6eab7b5e6a2c5641d64a90176d3d5510cd22672f
Z33xD <zeeshanmoinshariff@gmail.com>
2026-02-11 12:24:41 +0530

    Updated the `users` schema, and added the respective attributes to the `User` entity class

--- src/main/java/com/fintrack/finance_tracker/users/User.java
+++ src/main/java/com/fintrack/finance_tracker/users/User.java
@@ -21,12 +21,21 @@ public class User {
 
     private LocalDateTime created_at;
 
-    public User(int id, String username, String email, String password_hash, LocalDateTime created_at) {
+    private boolean enabled;
+
+    private String verification_code;
+
+    private LocalDateTime verification_expiration;
+
+    public User(int id, String username, String email, String password_hash, LocalDateTime created_at, boolean enabled, String verification_code, LocalDateTime verification_expiration) {
         this.id = id;
         this.username = username;
         this.email = email;
         this.password_hash = password_hash;
         this.created_at = created_at;
+        this.enabled = enabled;
+        this.verification_code = verification_code;
+        this.verification_expiration = verification_expiration;
     }
 
     public User() {}
@@ -70,4 +79,29 @@ public class User {
     public void setCreated_at(LocalDateTime created_at) {
         this.created_at = created_at;
     }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getVerification_code() {
+        return verification_code;
+    }
+
+    public void setVerification_code(String verification_code) {
+        this.verification_code = verification_code;
+    }
+
+    public LocalDateTime getVerification_expiration() {
+        return verification_expiration;
+    }
+
+    public void setVerification_expiration(LocalDateTime verification_expiration) {
+        this.verification_expiration = verification_expiration;
+    }
+
 }
