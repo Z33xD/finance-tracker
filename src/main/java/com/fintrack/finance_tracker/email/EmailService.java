6fd702344e993b895ded2a4a66693be1c227a1b7
Z33xD <zeeshanmoinshariff@gmail.com>
2026-04-05 13:42:42 +0530

    Setting the "from" email for the `EmailService`

--- src/main/java/com/fintrack/finance_tracker/email/EmailService.java
+++ src/main/java/com/fintrack/finance_tracker/email/EmailService.java
@@ -3,12 +3,16 @@ package com.fintrack.finance_tracker.email;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
     @Autowired
     private JavaMailSender emailSender;
 
@@ -16,6 +20,7 @@ public class EmailService {
         MimeMessage message = emailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
+        helper.setFrom(fromEmail);
         helper.setTo(to);
         helper.setSubject(subject);
         helper.setText(text, true);
